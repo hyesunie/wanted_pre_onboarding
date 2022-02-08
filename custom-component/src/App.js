7c65644171f63f28f4446d12777b25704f5a602c
hyesunie <hyesunie1025@gmail.com>
2022-02-09 00:22:19 +0900

    feat: toggle기능 구현 및 스타일 적용

--- custom-component/src/App.js
+++ custom-component/src/App.js
@@ -1,7 +1,82 @@
-function App() {
+import React, { useState } from "react";
+import "./App.css";
+
+function ToggleButton(props) {
+  const [state, setState] = useState(false);
+  const onStyleOverlay = {
+    transform: "translate3d(100%,0,0)",
+  };
+  const offStyleOverlay = {
+    transform: "translate3d(0,0,0)",
+  };
+  const onStyleButton = {
+    transform: "translate3d(198%,0,0)",
+  };
+  const offStyleButton = {
+    transform: "translate3d(0,0,0)",
+  };
+
+  const handleToggleButton = () => {
+    const newState = state ? false : true;
+    setState(newState);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">hello</header>
+    <div className="viewport__toggle">
+      <div className="viewport__title">Toggle</div>
+      <div className="viewport__context">
+        <div className="viewport__toggle__button-wrapper">
+          <div
+            className="toggle-overlay"
+            style={state ? onStyleOverlay : offStyleOverlay}
+          ></div>
+          <div
+            className="toggle-button"
+            onClick={() => {
+              handleToggleButton();
+            }}
+            style={state ? onStyleButton : offStyleButton}
+          ></div>
+        </div>
+        <h2>Toggle Switch {state ? "ON" : "OFF"}</h2>
+      </div>
+    </div>
+  );
+}
+
+function App() {
+  const [state, setState] = useState("init");
+
+  const router = {
+    init: () => {
+      return <h2> 위의 버튼을 눌러주세요. </h2>;
+    },
+    toggle: () => {
+      return <ToggleButton></ToggleButton>;
+    },
+  };
+
+  const handleClick = (e) => {
+    const key = e.target.id;
+    setState(key);
+  };
+
+  return (
+    <div className="app">
+      <header>Hyesunie Project</header>
+      <main>
+        <div className="button-wrapper">
+          <button
+            onClick={(e) => {
+              handleClick(e);
+            }}
+            id="toggle"
+          >
+            Toggle
+          </button>
+        </div>
+        <div className="viewport-wrapper">{router[state]()}</div>
+      </main>
     </div>
   );
 }
